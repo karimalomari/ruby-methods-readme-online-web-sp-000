62bf5546c73dd56e46633b7191a0f7f25de953d6
Karim Alomari <karim.alomari@gmail.com>
2020-07-09 15:48:08 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,15 +1,7 @@
 def say_hello_world_ten_times
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+    puts phrase
+    puts phrase
 end
 
 say_hello_world_ten_times
